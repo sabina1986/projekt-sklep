1d259315943abb7b32884cee5a72abca757fe24f
Sabina Kowalczyk <sabkow@vp.pl>
2011-05-12 21:03:34 +0200

    zmiany

--- Kopera/MasterPage.Master.designer.cs
+++ Kopera/MasterPage.Master.designer.cs
@@ -74,5 +74,14 @@ namespace Kopera {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// stopka1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Blog.Stopka stopka1;
     }
 }
